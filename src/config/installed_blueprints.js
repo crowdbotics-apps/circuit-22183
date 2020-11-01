73ac7f78c842313d1984a227f76ea3ea846c549e
Crowdbotics <team@crowdbotics.com>
2020-11-01 15:48:06 +0000

    add package blueprint-react-native-maps-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Maps4166177', human_name: 'Maps 4', access_route: 'Maps4166177', icon: 'map'},
 { name: 'EmailAuth5166176', human_name: 'Email Auth 5', access_route: 'EmailAuth5166176', icon: 'envelope-o'},
 { name: 'Feed6166175', human_name: 'Feed 6', access_route: 'Feed6166175'},
 { name: 'UserProfile7166174', human_name: 'User Profile 7', access_route: 'UserProfile7166174'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Maps4166177Navigator from '../features/Maps4166177/navigator';
 import EmailAuth5166176Navigator from '../features/EmailAuth5166176/navigator';
 import Feed6166175Navigator from '../features/Feed6166175/navigator';
 import UserProfile7166174Navigator from '../features/UserProfile7166174/navigator';
@@ -18,6 +19,7 @@ import SignUp29166172Navigator from '../features/SignUp29166172/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Maps4166177: { screen: Maps4166177Navigator },
 EmailAuth5166176: { screen: EmailAuth5166176Navigator },
 Feed6166175: { screen: Feed6166175Navigator },
 UserProfile7166174: { screen: UserProfile7166174Navigator },
