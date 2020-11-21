65cd08201f61f18e32e468c7e9f1b39f3a6826a9
Crowdbotics <team@crowdbotics.com>
2020-11-21 23:59:31 +0000

    add package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'UserProfile7177294', human_name: 'User Profile 7', access_route: 'UserProfile7177294'},
 { name: 'CalendarView3166179', human_name: 'Calendar View 3', access_route: 'CalendarView3166179', icon: 'calendar'},
 { name: 'Maps4166177', human_name: 'Maps 4', access_route: 'Maps4166177', icon: 'map'},
 { name: 'EmailAuth5166176', human_name: 'Email Auth 5', access_route: 'EmailAuth5166176', icon: 'envelope-o'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import UserProfile7177294Navigator from '../features/UserProfile7177294/navigator';
 import Feed6166175Navigator from '../features/Feed6166175/navigator';
 import UserProfile7166174Navigator from '../features/UserProfile7166174/navigator';
 import SignUp29166172Navigator from '../features/SignUp29166172/navigator';
@@ -16,6 +17,7 @@ import SignUp29166172Navigator from '../features/SignUp29166172/navigator';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+UserProfile7177294: { screen: UserProfile7177294Navigator },
 Feed6166175: { screen: Feed6166175Navigator },
 UserProfile7166174: { screen: UserProfile7166174Navigator },
 SignUp29166172: { screen: SignUp29166172Navigator },
